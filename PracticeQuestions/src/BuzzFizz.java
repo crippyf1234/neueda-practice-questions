f101a2ad7f2162f3268c54d0e404dc0e6fad8691
Chris Foster <s.boole@outlook.com>
2018-12-07 10:20:14 +0000

    Testing commit with completed problem BuzzFizz

---
+++ PracticeQuestions/src/BuzzFizz.java
@@ -0,0 +1,25 @@
+
+public class BuzzFizz {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i < 1001 ; i++) {
+			
+			if(i % 3 == 0 && i % 5 == 0) {
+				System.out.println("Buzz Fizz");
+			}
+			
+			else if(i % 3 == 0) {
+				System.out.println("Buzz");
+			}
+			
+			else if(i % 5 == 0) {
+				System.out.println("Fizz");
+			}
+			
+			else {System.out.println(i);}
+		}
+
+	}
+
+}
---
+++ PracticeQuestions/src/HigherorLower.java
@@ -0,0 +1,9 @@
+
+public class HigherorLower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
